fd7b75d76d9127362bf45f0c80cfd0206e9c0d3d
Mikhail <mikhail.sapuzhak@gmail.com>
2020-02-21 18:26:06 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,7 +3,45 @@
 const calendar = document.querySelector('#calendar');
 
 function calendarTable(year, month, element) {
-  // WRITE YOUR CODE HERE
+  const mon = month - 1;
+  const days = new Date(year, mon);
+
+  let table = `<table><tr><th>Пн</th><th>Вт</th><th>Ср</th>
+    <th>Чт</th><th>Пт</th><th>Сб</th><th>Вс</th></tr><tr>`;
+
+  function getDay(date) {
+    let day = date.getDay();
+
+    if (day === 0) {
+      day = 7;
+    }
+
+    return day - 1;
+  }
+
+  for (let i = 0; i < getDay(days); i++) {
+    table += `<td></td>`;
+  }
+
+  while (days.getMonth() === mon) {
+    table += `<td>${days.getDate()}</td>`;
+
+    if (getDay(days) % 7 === 6) {
+      table += `</tr><tr>`;
+    }
+
+    days.setDate(days.getDate() + 1);
+  }
+
+  if (getDay(days) !== 0) {
+    for (let i = getDay(days); i < 7; i++) {
+      table += `<td></td>`;
+    }
+  }
+
+  table += `</tr></table>`;
+
+  element.innerHTML = table;
 }
 
-calendarTable(2019, 10, calendar);
+calendarTable(2019, 11, calendar);
